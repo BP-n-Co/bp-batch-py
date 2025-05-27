8b4b6435d6bbc5dd9842c44787ec7825284f10f2
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-05-27 16:15:44 +0900

    Revert "Weekly Release: release/2025-05-27 (#12)"
    This reverts commit 122176c604cecc17e919850b48d35bc8e766b86c.

--- src/oneshot/fetchCommits/config.py
+++
@@ -1,7 +0,0 @@
-from _interface import get_logger
-
-# TODO: change to the one in _config if turned to batch
-ENV = "local"
-SILENT = False
-
-logger = get_logger(name="FetchCommitsLogger", env=ENV)
